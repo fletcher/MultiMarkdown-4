799637daf22067f84925065675330aa0c56b0cf1
Fletcher T. Penney <fletcher@fletcherpenney.net>
2013-12-02 15:56:54 -0500

    update licensing information per https://github.com/danielpunkass/peg-multimarkdown/commit/327c178e5706ed76b19f289e78192a3d3ea316fe

--- GLibFacade.h
+++ GLibFacade.h
@@ -3,7 +3,30 @@
  *	MultiMarkdown
  *	
  *	Created by Daniel Jalkut on 7/26/11.
- *	Copyright 2011 __MyCompanyName__. All rights reserved.
+ *	Modified by Fletcher T. Penney on 9/15/11.
+ *	Modified by Dan Lowe on 1/3/12.
+ *
+ *	License for original code by Daniel Jalkut:
+ *	
+ *	Copyright 2011 Daniel Jalkut. All rights reserved.
+ *	
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *	this software and associated documentation files (the “Software”), to deal in
+ *	the Software without restriction, including without limitation the rights to
+ *	use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ *	of the Software, and to permit persons to whom the Software is furnished to do
+ *	so, subject to the following conditions:
+ *	
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *	
+ *	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
  */
 
 #ifndef __MARKDOWN_GLIB_FACADE__
